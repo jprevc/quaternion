698798b84c178c9c57c029acf9197cecb153705c
jprevc <jost.prevc@gmail.com>
2018-12-24 13:53:18 +0100

    added additional info to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='quaterion',
       version='1.0',
-      description='Python quaternion library',
+      description='A simple and efficient library for computations with quaternions.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Jost Prevc',
       author_email='jost.prevc@gmail.com',
-      packages=['quaternion']
+      url="https://github.com/jprevc/quaternion",
+      packages=['quaternion'],
+      install_requires=['numpy'],
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
       )
