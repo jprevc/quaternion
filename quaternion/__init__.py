2d4abf7979c75dea932fe111c0c59ced7e907822
jprevc <jost.prevc@gmail.com>
2018-12-24 08:25:16 +0100

    added setup.py changed project structure -> created folder quaternion and moved quaternion.py to it added __init__.py

---
+++ quaternion/__init__.py
@@ -0,0 +1,2 @@
+from . import quaternion
+from .quaternion import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='quaterion',
+      version='1.0',
+      description='Python quaternion library',
+      author='Jost Prevc',
+      author_email='jost.prevc@gmail.com',
+      packages=['quaternion']
+      )
